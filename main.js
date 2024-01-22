fbabcffac4039fad0c9d810ec2e1a53c484f4735
Julia Lavagnini Maia <julia.lavagninimaia@gmail.com>
2024-01-22 17:47:16 +0000

    Create functions to add world clocks

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function displayDateTime(event){
+    if(event.target.value.length > 0){
+        let currentTime = moment().tz(event.target.value).format('dddd Do MMMM, hh:mm:s A')
+        console.log(currentTime);
+
+        let date = document.querySelector('#date');
+        date.innerHTML = currentTime;
+    };
+};
+
+
+let countrySelect = document.querySelector('#countries');
+countrySelect.addEventListener('change', displayDateTime);
+
